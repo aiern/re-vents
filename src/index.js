5e5d41390f9f90805c5bec214fa2d76a97178ddb
ai_ern <im_in@outlook.sg>
2021-01-29 16:59:33 +0800

    routing configurations and setup

--- src/index.js
+++ src/index.js
@@ -1,28 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css'
-import './app/layout/styles.css';
-import App from './app/layout/App.jsx';
+import React from "react";
+import ReactDOM from "react-dom";
+import "semantic-ui-css/semantic.min.css";
+import "./app/layout/styles.css";
+import App from "./app/layout/App.jsx";
 // import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { configureStore } from "./app/store/configureStore";
+import { BrowserRouter } from "react-router-dom";
 
-const rootEl = document.getElementById('root')
+const store = configureStore();
+
+console.log(store.getState());
+const rootEl = document.getElementById("root");
 
 function render() {
-  ReactDOM.render(<App />, rootEl);
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+    rootEl
+  );
 }
 
 if (module.hot) {
-  module.hot.accept('./app/layout/App.jsx', function () {
-
-    setTimeout(render)
-  })
+  module.hot.accept("./app/layout/App.jsx", function () {
+    setTimeout(render);
+  });
 }
 
-render()
+render();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
 
-serviceWorker.unregister()
+serviceWorker.unregister();
